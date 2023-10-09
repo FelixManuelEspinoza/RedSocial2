da77268671b4a01e79cbf00a4f181c544edd2765
FelixManuelEspinoza <81267662+FelixManuelEspinoza@users.noreply.github.com>
2023-10-09 07:54:27 -0300

    controllers

--- RedSocial/Server/Controllers/TrabajadorController.cs
+++ RedSocial/Server/Controllers/TrabajadorController.cs
@@ -23,6 +23,55 @@ namespace RedSocial.Server.Controllers
             return await dBcontext.Trabajadores.ToListAsync();
         }
 
+        [HttpPost]
+
+        public async Task<ActionResult<int>> Post(Trabajador trabajador)
+        {
+            dBcontext.Add(trabajador);
+            await dBcontext.SaveChangesAsync();
+            return trabajador.Id;
+
+
+
+
+        }
+
+        [HttpPut("{id:int}")]
+        public async Task<ActionResult> Put(Trabajador trabajador, int id)
+        {
+            if (id != trabajador.Id)
+            {
+                return BadRequest("El id del trabajador no corresponde.");
+            }
+
+            var existe = await dBcontext.Trabajadores.AnyAsync(x => x.Id == id);
+            if (!existe)
+            {
+                return NotFound($"el Trabajador de id={id} no existe");
+            }
+
+            dBcontext.Update(trabajador);
+            await dBcontext.SaveChangesAsync();
+            return Ok();
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var existe = await dBcontext.Trabajadores.AnyAsync(x => x.Id == id);
+            if (!existe)
+            {
+                return NotFound($"El trabajador de id={id} no existe");
+            }
+            Trabajador pepe = new Trabajador();
+            pepe.Id = id;
+
+            dBcontext.Remove(pepe);
+
+            await dBcontext.SaveChangesAsync();
+
+            return Ok();
+        }
 
     }
 }
