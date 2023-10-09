5bd345f2bb7caa9db4d6abae6267e882fe7d60d1
FelixManuelEspinoza <81267662+FelixManuelEspinoza@users.noreply.github.com>
2023-10-09 00:47:41 -0300

    Controller

---
+++ RedSocial/Server/Controllers/PublicacionController.cs
@@ -0,0 +1,26 @@
+﻿using BDRedSocial;
+using BDRedSocial.Data.Entity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace RedSocial.Server.Controllers
+{
+    [ApiController]
+    [Route("api/Publicacion")]
+    public class PublicacionController : ControllerBase
+    {
+        private readonly DBcontext dBcontext;
+
+        public PublicacionController(DBcontext dBcontext)
+        {
+            this.dBcontext = dBcontext;
+        }
+
+        [HttpGet]
+
+        public async Task<ActionResult<List<Publicacion>>> Get() 
+        {
+            return await dBcontext.PuBlicaciones.ToListAsync();
+        }
+    }
+}
---
+++ RedSocial/Server/Controllers/TrabajadorController.cs
@@ -0,0 +1,26 @@
+﻿using BDRedSocial;
+using BDRedSocial.Data.Entity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace RedSocial.Server.Controllers
+{
+    [ApiController]
+    [Route("api/Trabajador")]
+    public class TrabajadorController: ControllerBase
+    {
+        private readonly DBcontext dBcontext;
+
+        public TrabajadorController(DBcontext dBcontext)
+        {
+            this.dBcontext = dBcontext;
+        }
+
+        [HttpGet]
+
+        public async Task<ActionResult<List<Publicacion>>> Get()
+        {
+            return await dBcontext.PuBlicaciones.ToListAsync();
+        }
+    }
+}
