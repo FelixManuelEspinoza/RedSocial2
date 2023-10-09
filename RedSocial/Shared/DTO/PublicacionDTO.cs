4e326bb33c332b87d9ce8247a76eaedd2af9f98b
FelixManuelEspinoza <81267662+FelixManuelEspinoza@users.noreply.github.com>
2023-10-09 09:33:26 -0300

    DTO

---
+++ RedSocial/Shared/DTO/PublicacionDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedSocial.Shared.DTO
+{
+    internal class PublicacionDTO
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public string Texto { get; set; }
+        public string FormatoImagen { get; set; }
+        public DateTime FechaPublicacion { get; set; }
+        public int AutorId { get; set; } 
+    }
+}
